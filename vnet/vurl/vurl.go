40c4c4cb13dedc07e909a83359da91dde51d7767
wongoo <gelnyang@163.com>
2019-12-11 17:34:03 +0800

    vurl

---
+++ vnet/vurl/vurl.go
@@ -0,0 +1,20 @@
+// Copyright 2019 The vogo Authors. All rights reserved.
+
+package vurl
+
+import (
+	"net"
+	"net/url"
+)
+
+// IsURLNetError whether net error for url request
+func IsURLNetError(err error) bool {
+	urlErr, ok := err.(*url.Error)
+	if !ok {
+		return false
+	}
+
+	_, ok = urlErr.Err.(*net.OpError)
+
+	return ok
+}
