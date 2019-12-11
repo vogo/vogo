893e89a8d0d6ce21f506e7d8c5a73fc1ad4cece2
wongoo <gelnyang@163.com>
2019-12-11 17:52:28 +0800

    RoundValidFloat

--- vmath/vmath.go
+++ vmath/vmath.go
@@ -4,12 +4,21 @@ package vmath
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
 func RoundFloat(f float64) float64 {
 	v, _ := strconv.ParseFloat(fmt.Sprintf("%.4f", f), 64)
 	return v
+}
+
+func RoundValidFloat(f float64) float64 {
+	if math.IsNaN(f) {
+		return 0
+	}
+
+	return RoundFloat(f)
 }
 
 func RoundFloat64(f float64, precision int) float64 {
