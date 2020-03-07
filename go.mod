19b89e6b14e3a7ab97ae097bc02529db36b7e870
wongoo <gelnyang@163.com>
2020-03-07 10:58:38 +0800

    vsnowflake

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/vogo/vogo
 go 1.13
 
 require (
+	github.com/sony/sonyflake v1.0.0
 	github.com/stretchr/testify v1.4.0
 	github.com/vogo/logger v1.3.0
 )
---
+++ vdistribution/vsnowflake/vsnowflake.go
@@ -0,0 +1,80 @@
+package vsnowflake
+
+import (
+	"encoding/binary"
+	"net"
+	"time"
+
+	"github.com/sony/sonyflake"
+	"github.com/vogo/vogo/vnet"
+)
+
+var (
+	snowflakeStartTime = time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+	machineIDFromIP    uint16
+)
+
+// getUniqueMachineID  get the unique machine id
+// default get machine id from ip address.
+// NOTE: for a distributed system, it's better to register machine into a general center, which make sure the machine id is unique.
+func getUniqueMachineID() (uint16, error) {
+	if machineIDFromIP > 0 {
+		return machineIDFromIP, nil
+	}
+
+	ipString, err := vnet.LocalIP()
+	if err != nil {
+		return 0, err
+	}
+
+	ip := net.ParseIP(ipString)
+
+	machineIDFromIP = binary.BigEndian.Uint16(ip[len(ip)-2:])
+
+	return machineIDFromIP, nil
+}
+
+func newSnowflake() *sonyflake.Sonyflake {
+	return sonyflake.NewSonyflake(sonyflake.Settings{
+		// StartTime is the time since which the Sonyflake time is defined as the elapsed time.
+		// If StartTime is 0, the start time of the Sonyflake is set to "2014-09-01 00:00:00 +0000 UTC".
+		// If StartTime is ahead of the current time, Sonyflake is not created.
+		StartTime: snowflakeStartTime,
+
+		// MachineID returns the unique ID of the Sonyflake instance.
+		// If MachineID returns an error, Sonyflake is not created.
+		// If MachineID is nil, default MachineID is used.
+		// Default MachineID returns the lower 16 bits of the private IP address.
+		MachineID: getUniqueMachineID,
+
+		// CheckMachineID validates the uniqueness of the machine ID.
+		// If CheckMachineID returns false, Sonyflake is not created.
+		// If CheckMachineID is nil, no validation is done.
+		CheckMachineID: nil,
+	})
+}
+
+// Snowflake snow flake id interface
+type Snowflake interface {
+	NextID() uint64
+}
+
+type sonySnowflake struct {
+	flake *sonyflake.Sonyflake
+}
+
+// NextID can continue to generate IDs for about 174 years from StartTime.
+// But after the Sonyflake time is over the limit, NextID panics.
+func (s *sonySnowflake) NextID() uint64 {
+	id, err := s.flake.NextID()
+	if err != nil {
+		panic(err)
+	}
+
+	return id
+}
+
+// New Snowflake
+func New() Snowflake {
+	return &sonySnowflake{flake: newSnowflake()}
+}
