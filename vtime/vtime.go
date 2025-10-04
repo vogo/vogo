db3ab80e03bc25243b772b422c1a958e92fd815d
tiltwind <tiltwind@163.com>
2025-10-04 10:41:14 +0800

    StartOfDay EndOfDay

--- vtime/vtime.go
+++ vtime/vtime.go
@@ -130,3 +130,11 @@ func DateHourMinuteString(t *time.Time) string {
 
 	return t.Format(DateHourMinuteLayout)
 }
+
+func StartOfDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+}
+
+func EndOfDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 999999999, t.Location())
+}
